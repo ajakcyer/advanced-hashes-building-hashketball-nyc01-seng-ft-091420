61b739875aa2d54b964440e20facb8371d842b70
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-07 20:06:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -75,7 +75,7 @@ def game_hash
                               :shoe => 18,
                               :points => 10,
                               :rebounds => 1,
-                              :assists => 2,
+                              :assists => 1,
                               :steals => 2,
                               :blocks => 7,
                               :slam_dunks => 2
